5dab22ab8555bdedb7e3a342f5030236fa35251f
Abdulrasaq Nasirudeen <nosisky@gmail.com>
2017-07-31 11:54:54 +0100

    I added some test cases for invalid array in the tests.js file

--- tests.js
+++ tests.js
@@ -1,1 +1,32 @@
-tests
+'use strict'
+
+var chai = require('chai');
+var assert = chai.assert;
+
+var lib = require('./lib/library.js');
+
+describe("Test that a valid array was supplied", function() {
+  it("should return Please supply a valid array for Andela ", function() {
+    assert(
+      lib.arithGeo("Andela"), "Please supply a valid array")
+  });
+  it("should return Please supply a valid array for 123 ", function() {
+    assert(
+      lib.arithGeo(123), "Please supply a valid array")
+  });
+  it("should return Please supply a valid array for false ", function() {
+    assert(
+      lib.arithGeo(false), "Please supply a valid array")
+  });
+  it("should return Please supply a valid array for {}} ", function() {
+    assert(
+      lib.arithGeo({}), "Please supply a valid array")
+  });
+});
+
+describe("Test for invalid array length", function() {
+  it("should return 0 for empty array", function() {
+    assert(
+      lib.arithGeo([]), 0)
+  });
+});
